16084fc272337326f48d12dd5650bfaf2dcea049
Uday Singh <udaysingh.code@gmail.com>
2014-12-30 14:51:33 +0530

    authorization for link posting complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141230081808) do
+ActiveRecord::Schema.define(version: 20141230083921) do
 
   create_table "links", force: true do |t|
     t.string   "title"
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "links", ["user_id"], name: "index_links_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
